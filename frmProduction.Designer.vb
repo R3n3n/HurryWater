0cde176674e2563bb7a123d9105633aefc948a19
Renen <Renen@DESKTOP-1B94TU9>
2026-09-23 23:54:02 +0800

    Production Form

--- frmProduction.Designer.vb
+++ frmProduction.Designer.vb
@@ -24,24 +24,23 @@ Partial Class frmProduction
     Private Sub InitializeComponent()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.lblCustomers = New System.Windows.Forms.Label()
-        Me.dtpOrderDate = New System.Windows.Forms.DateTimePicker()
+        Me.dtpLogDate = New System.Windows.Forms.DateTimePicker()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.cboPickupStatus = New System.Windows.Forms.ComboBox()
+        Me.cboTank = New System.Windows.Forms.ComboBox()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.nudQuantity1 = New System.Windows.Forms.NumericUpDown()
+        Me.nudLiters = New System.Windows.Forms.NumericUpDown()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label12 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblBottles500 = New System.Windows.Forms.Label()
+        Me.lblBottles350 = New System.Windows.Forms.Label()
+        Me.lblGallons = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
-        Me.Label10 = New System.Windows.Forms.Label()
-        Me.Label11 = New System.Windows.Forms.Label()
-        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
         Me.btnSave = New System.Windows.Forms.Button()
-        CType(Me.nudQuantity1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudLiters, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -65,14 +64,14 @@ Partial Class frmProduction
         Me.lblCustomers.TabIndex = 6
         Me.lblCustomers.Text = "Log Production"
         '
-        'dtpOrderDate
+        'dtpLogDate
         '
-        Me.dtpOrderDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.dtpOrderDate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.dtpOrderDate.Location = New System.Drawing.Point(35, 180)
-        Me.dtpOrderDate.Name = "dtpOrderDate"
-        Me.dtpOrderDate.Size = New System.Drawing.Size(255, 38)
-        Me.dtpOrderDate.TabIndex = 44
+        Me.dtpLogDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpLogDate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpLogDate.Location = New System.Drawing.Point(35, 180)
+        Me.dtpLogDate.Name = "dtpLogDate"
+        Me.dtpLogDate.Size = New System.Drawing.Size(255, 38)
+        Me.dtpLogDate.TabIndex = 44
         '
         'Label3
         '
@@ -84,15 +83,15 @@ Partial Class frmProduction
         Me.Label3.TabIndex = 43
         Me.Label3.Text = "Date"
         '
-        'cboPickupStatus
+        'cboTank
         '
-        Me.cboPickupStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cboPickupStatus.FormattingEnabled = True
-        Me.cboPickupStatus.Items.AddRange(New Object() {"Pending", "Picked-up", "Delivered"})
-        Me.cboPickupStatus.Location = New System.Drawing.Point(327, 179)
-        Me.cboPickupStatus.Name = "cboPickupStatus"
-        Me.cboPickupStatus.Size = New System.Drawing.Size(323, 39)
-        Me.cboPickupStatus.TabIndex = 46
+        Me.cboTank.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboTank.FormattingEnabled = True
+        Me.cboTank.Items.AddRange(New Object() {"Pending", "Picked-up", "Delivered"})
+        Me.cboTank.Location = New System.Drawing.Point(327, 179)
+        Me.cboTank.Name = "cboTank"
+        Me.cboTank.Size = New System.Drawing.Size(323, 39)
+        Me.cboTank.TabIndex = 46
         '
         'Label4
         '
@@ -104,13 +103,13 @@ Partial Class frmProduction
         Me.Label4.TabIndex = 45
         Me.Label4.Text = "Tank"
         '
-        'nudQuantity1
+        'nudLiters
         '
-        Me.nudQuantity1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.nudQuantity1.Location = New System.Drawing.Point(35, 288)
-        Me.nudQuantity1.Name = "nudQuantity1"
-        Me.nudQuantity1.Size = New System.Drawing.Size(615, 38)
-        Me.nudQuantity1.TabIndex = 47
+        Me.nudLiters.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nudLiters.Location = New System.Drawing.Point(35, 288)
+        Me.nudLiters.Name = "nudLiters"
+        Me.nudLiters.Size = New System.Drawing.Size(615, 38)
+        Me.nudLiters.TabIndex = 47
         '
         'Label1
         '
@@ -125,9 +124,9 @@ Partial Class frmProduction
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.Gainsboro
-        Me.Panel1.Controls.Add(Me.Label11)
-        Me.Panel1.Controls.Add(Me.Label10)
-        Me.Panel1.Controls.Add(Me.Label9)
+        Me.Panel1.Controls.Add(Me.lblBottles500)
+        Me.Panel1.Controls.Add(Me.lblBottles350)
+        Me.Panel1.Controls.Add(Me.lblGallons)
         Me.Panel1.Controls.Add(Me.Label8)
         Me.Panel1.Controls.Add(Me.Label7)
         Me.Panel1.Controls.Add(Me.Label6)
@@ -138,45 +137,35 @@ Partial Class frmProduction
         Me.Panel1.Size = New System.Drawing.Size(615, 164)
         Me.Panel1.TabIndex = 49
         '
-        'Label12
+        'lblBottles500
         '
-        Me.Label12.AutoSize = True
-        Me.Label12.BackColor = System.Drawing.Color.Transparent
-        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label12.Location = New System.Drawing.Point(737, 17)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(0, 24)
-        Me.Label12.TabIndex = 27
+        Me.lblBottles500.AutoSize = True
+        Me.lblBottles500.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBottles500.Location = New System.Drawing.Point(434, 107)
+        Me.lblBottles500.Name = "lblBottles500"
+        Me.lblBottles500.Size = New System.Drawing.Size(29, 31)
+        Me.lblBottles500.TabIndex = 56
+        Me.lblBottles500.Text = "0"
         '
-        'Label5
+        'lblBottles350
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(17, 17)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(245, 24)
-        Me.Label5.TabIndex = 50
-        Me.Label5.Text = "Auto-Calculated Equivalents"
+        Me.lblBottles350.AutoSize = True
+        Me.lblBottles350.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBottles350.Location = New System.Drawing.Point(212, 107)
+        Me.lblBottles350.Name = "lblBottles350"
+        Me.lblBottles350.Size = New System.Drawing.Size(29, 31)
+        Me.lblBottles350.TabIndex = 55
+        Me.lblBottles350.Text = "0"
         '
-        'Label6
+        'lblGallons
         '
-        Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(17, 73)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(73, 24)
-        Me.Label6.TabIndex = 51
-        Me.Label6.Text = "Gallons"
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(214, 73)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(125, 24)
-        Me.Label7.TabIndex = 52
-        Me.Label7.Text = "350mL bottles"
+        Me.lblGallons.AutoSize = True
+        Me.lblGallons.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGallons.Location = New System.Drawing.Point(15, 107)
+        Me.lblGallons.Name = "lblGallons"
+        Me.lblGallons.Size = New System.Drawing.Size(29, 31)
+        Me.lblGallons.TabIndex = 54
+        Me.lblGallons.Text = "0"
         '
         'Label8
         '
@@ -188,48 +177,45 @@ Partial Class frmProduction
         Me.Label8.TabIndex = 53
         Me.Label8.Text = "500mL bottles"
         '
-        'Label9
+        'Label7
         '
-        Me.Label9.AutoSize = True
-        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label9.Location = New System.Drawing.Point(15, 107)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(29, 31)
-        Me.Label9.TabIndex = 54
-        Me.Label9.Text = "0"
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(214, 73)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(125, 24)
+        Me.Label7.TabIndex = 52
+        Me.Label7.Text = "350mL bottles"
         '
-        'Label10
+        'Label6
         '
-        Me.Label10.AutoSize = True
-        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label10.Location = New System.Drawing.Point(212, 107)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(29, 31)
-        Me.Label10.TabIndex = 55
-        Me.Label10.Text = "0"
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(17, 73)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(73, 24)
+        Me.Label6.TabIndex = 51
+        Me.Label6.Text = "Gallons"
         '
-        'Label11
+        'Label5
         '
-        Me.Label11.AutoSize = True
-        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label11.Location = New System.Drawing.Point(434, 107)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(29, 31)
-        Me.Label11.TabIndex = 56
-        Me.Label11.Text = "0"
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(17, 17)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(245, 24)
+        Me.Label5.TabIndex = 50
+        Me.Label5.Text = "Auto-Calculated Equivalents"
         '
-        'btnCancel
+        'Label12
         '
-        Me.btnCancel.BackColor = System.Drawing.Color.White
-        Me.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCancel.ForeColor = System.Drawing.Color.Black
-        Me.btnCancel.Location = New System.Drawing.Point(376, 562)
-        Me.btnCancel.Name = "btnCancel"
-        Me.btnCancel.Size = New System.Drawing.Size(109, 42)
-        Me.btnCancel.TabIndex = 51
-        Me.btnCancel.Text = "Cancel"
-        Me.btnCancel.UseVisualStyleBackColor = False
+        Me.Label12.AutoSize = True
+        Me.Label12.BackColor = System.Drawing.Color.Transparent
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(737, 17)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(0, 24)
+        Me.Label12.TabIndex = 27
         '
         'btnSave
         '
@@ -250,20 +236,19 @@ Partial Class frmProduction
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1072, 694)
-        Me.Controls.Add(Me.btnCancel)
         Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.nudQuantity1)
-        Me.Controls.Add(Me.cboPickupStatus)
+        Me.Controls.Add(Me.nudLiters)
+        Me.Controls.Add(Me.cboTank)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.dtpOrderDate)
+        Me.Controls.Add(Me.dtpLogDate)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.lblCustomers)
         Me.Name = "frmProduction"
         Me.Text = "frmProduction"
-        CType(Me.nudQuantity1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudLiters, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
@@ -273,21 +258,20 @@ Partial Class frmProduction
 
     Friend WithEvents Label2 As Label
     Friend WithEvents lblCustomers As Label
-    Friend WithEvents dtpOrderDate As DateTimePicker
+    Friend WithEvents dtpLogDate As DateTimePicker
     Friend WithEvents Label3 As Label
-    Friend WithEvents cboPickupStatus As ComboBox
+    Friend WithEvents cboTank As ComboBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents nudQuantity1 As NumericUpDown
+    Friend WithEvents nudLiters As NumericUpDown
     Friend WithEvents Label1 As Label
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Label6 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label12 As Label
-    Friend WithEvents Label11 As Label
-    Friend WithEvents Label10 As Label
-    Friend WithEvents Label9 As Label
+    Friend WithEvents lblBottles500 As Label
+    Friend WithEvents lblBottles350 As Label
+    Friend WithEvents lblGallons As Label
     Friend WithEvents Label8 As Label
     Friend WithEvents Label7 As Label
-    Friend WithEvents btnCancel As Button
     Friend WithEvents btnSave As Button
 End Class
--- frmProduction.vb
+++ frmProduction.vb
@@ -1,3 +1,101 @@
-﻿Public Class frmProduction
+﻿Imports MySql.Data.MySqlClient
+
+Public Class frmProduction
+
+    Private Const LITERS_PER_GALLON As Decimal = 11D
+    Private Const LITERS_PER_350ML As Decimal = 0.35D
+    Private Const LITERS_PER_500ML As Decimal = 0.5D
+
+    Private Sub frmProduction_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Liters input
+        nudLiters.DecimalPlaces = 2
+        nudLiters.Minimum = 0
+        nudLiters.Maximum = 1000000
+        nudLiters.Increment = 10
+        nudLiters.Value = 0
+
+        ' Tank list (the Designer still has the old Pending/Picked-up items, so clear them)
+        cboTank.DropDownStyle = ComboBoxStyle.DropDownList
+        cboTank.Items.Clear()
+        cboTank.Items.AddRange({"Tank-1", "Tank-2"})   ' edit to match your real tanks
+        cboTank.SelectedIndex = 0
+
+        dtpLogDate.Value = Date.Today
+
+        UpdateEquivalents()
+    End Sub
+
+    ' ---------- Live preview of equivalents ----------
+
+    Private Sub nudLiters_ValueChanged(sender As Object, e As EventArgs) Handles nudLiters.ValueChanged
+        UpdateEquivalents()
+    End Sub
+
+    Private Function GetGallons(liters As Decimal) As Decimal
+        Return Math.Round(liters / LITERS_PER_GALLON, 2)
+    End Function
+
+    ' Whole bottles only (partial bottles can't be sold)
+    Private Function GetBottles(liters As Decimal, litersPerBottle As Decimal) As Integer
+        Return CInt(Math.Floor(liters / litersPerBottle))
+    End Function
+
+    Private Sub UpdateEquivalents()
+        Dim liters As Decimal = nudLiters.Value
+        lblGallons.Text = GetGallons(liters).ToString("N2")
+        lblBottles350.Text = GetBottles(liters, LITERS_PER_350ML).ToString("N0")
+        lblBottles500.Text = GetBottles(liters, LITERS_PER_500ML).ToString("N0")
+    End Sub
+
+    ' ---------- Save ----------
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If CurrentUserId <= 0 Then
+            MsgBox("No logged-in user. Please log in again.", MsgBoxStyle.Exclamation, "Not logged in")
+            Return
+        End If
+
+        If cboTank.SelectedIndex = -1 Then
+            MsgBox("Select a tank.", MsgBoxStyle.Exclamation, "Validation error")
+            cboTank.Focus()
+            Return
+        End If
+
+        If nudLiters.Value <= 0 Then
+            MsgBox("Enter the liters produced (greater than 0).", MsgBoxStyle.Exclamation, "Validation error")
+            nudLiters.Focus()
+            Return
+        End If
+
+        Dim liters As Decimal = nudLiters.Value
+
+        Try
+            connection()
+            sql = "INSERT INTO tblproductionlog " &
+                  "(logDate, tankId, litersProduced, gallonEquivalent, bottle350mlEquivalent, bottle500mlEquivalent, recordedBy) " &
+                  "VALUES (@logDate, @tankId, @liters, @gallons, @b350, @b500, @recordedBy)"
+            cmd = New MySqlCommand(sql, cn)
+            cmd.Parameters.AddWithValue("@logDate", dtpLogDate.Value.Date)
+            cmd.Parameters.AddWithValue("@tankId", cboTank.Text)
+            cmd.Parameters.AddWithValue("@liters", liters)
+            cmd.Parameters.AddWithValue("@gallons", GetGallons(liters))
+            cmd.Parameters.AddWithValue("@b350", GetBottles(liters, LITERS_PER_350ML))
+            cmd.Parameters.AddWithValue("@b500", GetBottles(liters, LITERS_PER_500ML))
+            cmd.Parameters.AddWithValue("@recordedBy", CurrentUserId)
+            cmd.ExecuteNonQuery()
+
+            MsgBox("Production log saved.", MsgBoxStyle.Information, "Saved")
+            nudLiters.Value = 0          ' reset for the next entry; date and tank stay
+            nudLiters.Focus()
+        Catch ex As Exception
+            MsgBox("Database error: " & ex.Message, MsgBoxStyle.Critical, "Error")
+        Finally
+            If cn.State = ConnectionState.Open Then cn.Close()
+        End Try
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs)
+        Me.Close()
+    End Sub
 
 End Class
